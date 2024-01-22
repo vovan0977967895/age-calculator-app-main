465e6afbbf4db6fa70c12af6cd716296eadf4c4e
Volodymir <151960820+vovan0977967895@users.noreply.github.com>
2024-01-22 22:13:45 +0200

    тест ввода

--- js/main.js
+++ js/main.js
@@ -1,46 +1,55 @@
 'use strict'
-/* window.addEventListener('DOMContentLoaded', () =>{
-    
-}); */
-
-let dayInput = +document.querySelector('.day__input').value;
-    let monthInput = +document.querySelector('.month__input').value;
-    let yearInput = +document.querySelector('.year__input').value;
-    
-    let birthday = `${yearInput}-${monthInput}-${dayInput}`;
-    console.log(birthday);  
-    
-    function yearsPassed (timeBirthday){
-        let days, months, years;
-        let t = Date.parse(new Date()) - Date.parse(timeBirthday); 
-        console.log(t);
- /*   if (t <= 0){
-      years = 0;
-      months = 0;
-      days = 0;
-      hours = 0;
-      minutes = 0;
-      seconds = 0;
-   }else{ */
-        years = Math.floor( t / (12*30*1000 * 60 * 60 * 24));
-       /*  months =Mafh.floor( t / (30*1000 * 60 * 60 * 24)),
-        days = Math.floor( t / (1000 * 60 * 60 * 24)),
-        hours = Math.floor((t / (1000 * 60 * 60 ) % 24)),
-        minutes = Math.floor((t / 1000 / 60) % 60),
-        seconds = Math.floor((t / 1000) % 60); */
+window.addEventListener('DOMContentLoaded', () =>{
+   let dayInput = +document.querySelector('.day__input').value;
+   let monthInput = +document.querySelector('.month__input').value;
+   let yearInput = +document.querySelector('.year__input').value;
+   let btnInput = document.querySelector('.btn__input')
+   let birthday = '';
+   if (monthInput > 0 && monthInput < 10  ){
+      birthday = `${yearInput}-0${monthInput}-${dayInput}`;
+   }else{
+      birthday = `${yearInput}-${monthInput}-${dayInput}`;
+   }
+   console.log(birthday);  
+   console.log(new Date());
    
-            return years;
- /*   return {
-      'total': t,
-      'years': years,
-      'months': months,
-      'days': days,
-      'hours': hours,
-      'minutes': minutes,
-      'seconds': seconds
-   }   */
-}
+   document.querySelector('.years__num').innerHTML = '--'
+   document.querySelector('.months__num').innerHTML = '--'
+   document.querySelector('.days__num').innerHTML = '--'
+   function oldOut (input){
+      let days = 0;
+      let months = 0;
+      let years = 0;
+      const t = Date.parse(new Date()) - Date.parse(input); 
+      console.log(t);
+      years = Math.floor( t / (365.25 * 1000 * 60 * 60 * 24)),
+      months = Math.floor((t - (365.25 * 1000 * 60 * 60 * 24) * years) / (3600*1000*24*(365.25/12))),
+      days = Math.floor((((t - (365.25 * 1000 * 60 * 60 * 24) * years)) - months*(3600*1000*24*(365.25/12))) /(3600*1000*24)), 
+         
+      document.querySelector('.years__num').innerHTML = years
+      document.querySelector('.months__num').innerHTML = months
+      document.querySelector('.days__num').innerHTML = days
+      console.log(years);
+      console.log(months);
+      console.log(days);
+      
+      return {
+         'total': t,
+         'years': years,
+         'months': months,
+         'days': days     
+      }
+   }
+   btnInput.onclick = function() {
+      oldOut(birthday);
+   }  
     
-    console.log(yearsPassed (birthday));
+});
+
+
+
+
+
+
     
-    document.querySelector('.years__num').innerHTML = yearsPassed (birthday)
+    
